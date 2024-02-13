0f7e6dfff2324b779bcf5c6acf3386d1d76455b7
Rezi Apriliansyah <Reziapriliansyah@gmail.com>
2024-02-13 23:02:23 +0700

    Update Home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 class Home extends MY_Controller
 {
@@ -22,46 +22,48 @@ class Home extends MY_Controller
         $this->data['home_img'] = $this->Home_image_m->get();
 
         $this->data['content'] = 'home';
-        $this->data['title'] = 'Home | '.$this->title;
-        $this->template($this->data ,'home');
+        $this->data['title'] = 'Home | ' . $this->title;
+        $this->template($this->data, 'home');
     }
 
-    
+
     public function product()
     {
         $this->data['products'] = $this->Product_m->get();
 
         $this->data['content'] = 'produk';
-        $this->data['title'] = 'Home | '.$this->title;
-        $this->template($this->data ,'home');
+        $this->data['title'] = 'Home | ' . $this->title;
+        $this->template($this->data, 'home');
     }
 
-    
+
     public function product_detail($id)
     {
         $this->data['product'] = $this->Product_m->get_row(['id' => $id]);
         $image[] = $this->data['product']->foto;
         $a = json_decode($this->data['product']->galery);
-        if(count($a) > 0) {
-            $this->data['product']->image = array_merge($image ,  $a);
+        if (count($a) > 0) {
+            $this->data['product']->image = array_merge($image, $a);
         } else {
             $this->data['product']->image = $image;
         }
-        
+
         $this->data['content'] = 'product-detail';
-        $this->data['title'] = 'Home | '.$this->title;
-        $this->template($this->data ,'home');
-    }    
+        $this->data['title'] = 'Home | ' . $this->title;
+        $this->template($this->data, 'home');
+    }
 
     public function about()
     {
         $this->data['about'] = $this->Setting_m->get_row(['index' => 'tentang'])->value;
         $docs = $this->Setting_m->get_row(['index' => 'document_pendukung'])->value;
-        $this->data['docs'] = json_decode($docs, true);
-
+        $this->data['docs'] = [];
+        if (!empty($docs)) {
+            $this->data['docs'] = json_decode($docs, true);
+        }
         $this->data['content'] = 'about';
-        $this->data['title'] = 'About | '.$this->title;
-        $this->template($this->data ,'home');
+        $this->data['title'] = 'About | ' . $this->title;
+        $this->template($this->data, 'home');
     }
 
 }
